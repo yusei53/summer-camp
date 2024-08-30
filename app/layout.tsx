6c4bc248ec7ae8bbf028a881a28d4d1b92e9bbf2
yusei53 <yusei530922@gmail.com>
2024-08-30 21:28:53 +0900

    nextjsの初期化

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import { Container } from "@mui/material";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body style={{ margin: 0 }}>
+        <Container maxWidth="md">{children}</Container>
+      </body>
     </html>
   );
 }
