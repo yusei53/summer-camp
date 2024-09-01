eb532f1c2d84d8b9100f24bc8ccc0de7e96abd35
togoai0930@icloud.com <togoai0930@icloud.com>
2024-09-02 02:52:19 +0900

    リンクタグを追加してカード一覧のページを作成した

---
+++ app/card/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+const CardComponent: React.FC = () => {
+  return <p>カード一覧</p>;
+};
+
+export default CardComponent;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+"use client";
 import getCurrentUser from "./actions/getCurrentUser";
 import GoogleLoginForm from "./components/auth/GoogleLoginForm";
 import LogoutButton from "./components/auth/LogoutButton";
+import Link from "next/link";
 
 const Home = async () => {
   const currentUser = await getCurrentUser();
@@ -8,6 +10,10 @@ const Home = async () => {
     <>
       <div>ログイン中: ログインしてた時のUIは後で考える</div>
       <LogoutButton />
+
+      <Link href="/card" color={"black"}>
+        カード一覧
+      </Link>
     </>
   ) : (
     <GoogleLoginForm />
