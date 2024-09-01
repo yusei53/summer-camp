e03a120e1afc7a2ef03bfa65b4ba56efb65ce1a8
yusei53 <yusei530922@gmail.com>
2024-09-02 00:23:37 +0900

    update: コンポーネントを反映

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import getCurrentUser from "./actions/getCurrentUser";
 import GoogleLoginForm from "./components/auth/GoogleLoginForm";
 import LogoutButton from "./components/auth/LogoutButton";
+import GroupFormContainer from "./components/group/GroupFormContainer";
 
 const Home = async () => {
   const currentUser = await getCurrentUser();
@@ -8,6 +9,7 @@ const Home = async () => {
     <>
       <div>ログイン中: ログインしてた時のUIは後で考える</div>
       <LogoutButton />
+      <GroupFormContainer />
     </>
   ) : (
     <GoogleLoginForm />
