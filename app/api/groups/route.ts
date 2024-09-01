3201b1766b234642349fcc715eda91ca45afe203
yusei53 <yusei530922@gmail.com>
2024-09-01 23:44:04 +0900

    fix: route.tsの修正

--- app/api/groups/route.ts
+++ app/api/groups/route.ts
@@ -1,47 +1,25 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getServerSession } from "next-auth";
-import authOptions from "../auth/[...nextauth]/options";
 import prisma from "@/app/lib/prisma";
-
-// 型定義の拡張
-declare module "next-auth" {
-  interface Session {
-    user: {
-      id: string;
-      name?: string | null;
-      email?: string | null;
-      image?: string | null;
-    };
-  }
-}
+import getCurrentUser from "@/app/actions/getCurrentUser";
 
 export async function POST(req: NextRequest) {
-  const session = await getServerSession(authOptions);
-
-  if (!session) {
-    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-  }
-
-  const userId = session?.user?.id;
-
   try {
     const { groupName } = await req.json();
 
-    if (!groupName) {
-      return NextResponse.json(
-        { message: "Group name is required" },
-        { status: 400 }
-      );
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser?.id || !currentUser?.email) {
+      return new NextResponse("認証されていません", { status: 401 });
     }
 
-    const newGroup = await prisma.group.create({
+    const response = await prisma.group.create({
       data: {
         groupName,
-        userId,
+        userId: currentUser.id,
       },
     });
 
-    return NextResponse.json(newGroup, { status: 201 });
+    return NextResponse.json(response, { status: 201 });
   } catch (error) {
     console.error(error);
     return NextResponse.json(
