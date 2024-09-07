b3dbf4849f99a0e91f50ad4d96ea2b64eaa88cd0
yusei53 <yusei530922@gmail.com>
2024-09-07 19:07:45 +0900

    GroupList.tsxの作成

---
+++ app/components/group/GroupList.tsx
@@ -0,0 +1,20 @@
+import getGroups from "@/app/actions/getGroups";
+import { Box } from "@mui/material";
+import { User } from "@prisma/client";
+import React from "react";
+
+type TProps = {
+  currentUserId: User["id"] | null;
+};
+
+const GroupList: React.FC<TProps> = async ({ currentUserId }) => {
+  if (!currentUserId) return;
+  const groups = await getGroups(currentUserId);
+  return groups ? (
+    groups.map((group) => <div key={group.id}>{group.groupName}</div>)
+  ) : (
+    <Box>グループがありません</Box>
+  );
+};
+
+export default GroupList;
