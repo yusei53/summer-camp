72bdfa431d715661717d84e68c5921e5d0eece7d
togoai0930@icloud.com <togoai0930@icloud.com>
2024-09-07 16:48:46 +0900

    cardtitle/cardcontentをキャメルに修正/widthを削除/export default functionをconstに修正

--- app/components/card/CardDescription.tsx
+++ app/components/card/CardDescription.tsx
@@ -6,42 +6,41 @@ import { posts } from "@/app/data/data";
 // Post型の定義
 interface Post {
   id: number;
-  cardtitle: string;
-  cardcontent: string;
+  CardTitle: string;
+  CardContent: string;
 }
 
-export default function CardDescription() {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
+const CardDescription = () => {
+  const [IsModalOpen, setIsModalOpen] = useState(false);
   const [selectedPost, setSelectedPost] = useState<Post | null>(null);
 
   const handleOpenModal = (post: Post) => {
     setSelectedPost(post);
-    setModalIsOpen(true);
+    setIsModalOpen(true);
   };
 
   const handleCloseModal = () => {
-    setModalIsOpen(false);
+    setIsModalOpen(false);
     setSelectedPost(null);
   };
 
   return (
     <Box>
       {posts.map((post) => (
-        <Box key={post.id} onClick={() => handleOpenModal(post)} sx={{ mr: 2 }}>
+        <Box key={post.id} onClick={() => handleOpenModal(post)} mr={2}>
           <Box fontSize={20} fontWeight="bold" m={3}>
-            {post.cardtitle}
+            {post.CardTitle}
           </Box>
         </Box>
       ))}
 
-      <Modal open={modalIsOpen} onClose={handleCloseModal}>
+      <Modal open={IsModalOpen} onClose={handleCloseModal}>
         <Box
           sx={{
             position: "absolute",
             top: "50%",
             left: "50%",
             transform: "translate(-50%, -50%)",
-            width: 400,
             bgcolor: "white",
             p: 4,
           }}
@@ -49,12 +48,14 @@ export default function CardDescription() {
           {selectedPost && (
             <ItemCardDescription
               id={selectedPost.id}
-              cardtitle={selectedPost.cardtitle}
-              cardcontent={selectedPost.cardcontent}
+              CardTitle={selectedPost.CardTitle}
+              CardContent={selectedPost.CardContent}
             />
           )}
         </Box>
       </Modal>
     </Box>
   );
-}
+};
+
+export default CardDescription;
