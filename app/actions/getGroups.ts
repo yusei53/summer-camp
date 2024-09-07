653a80e79d2f60b0029eb2d0f73a1384708daba4
yusei53 <yusei530922@gmail.com>
2024-09-07 19:08:19 +0900

    getGroups.tsの作成

---
+++ app/actions/getGroups.ts
@@ -0,0 +1,18 @@
+import prisma from "@/app/lib/prisma";
+
+const getGroups = async (userId: string) => {
+  try {
+    const groups = await prisma.group.findMany({
+      where: { userId },
+      select: {
+        id: true,
+        groupName: true,
+      },
+    });
+    return groups;
+  } catch (error) {
+    console.error("Error fetching groups:", error);
+  }
+};
+
+export default getGroups;
