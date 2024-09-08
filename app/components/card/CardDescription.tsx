bf73a641f06c55db49148fc4eae7e6bb8830fbf8
togoai0930@icloud.com <togoai0930@icloud.com>
2024-09-09 00:06:08 +0900

    スクロールバーを追加した

--- app/components/card/CardDescription.tsx
+++ app/components/card/CardDescription.tsx
@@ -3,58 +3,49 @@ import { Box, Modal } from "@mui/material";
 import ItemCardDescription from "./ItemCardDescription";
 import { posts } from "@/app/data/data";
 
-// Post型の定義
-interface Post {
-  id: number;
-  CardTitle: string;
-  CardContent: string;
-}
-
 const CardDescription = () => {
   const [IsModalOpen, setIsModalOpen] = useState(false);
-  const [selectedPost, setSelectedPost] = useState<Post | null>(null);
 
-  const handleOpenModal = (post: Post) => {
-    setSelectedPost(post);
+  const handleOpenModal = () => {
     setIsModalOpen(true);
   };
 
   const handleCloseModal = () => {
     setIsModalOpen(false);
-    setSelectedPost(null);
   };
 
   return (
     <Box position="relative">
       {posts.map((post) => (
-        <Box key={post.id} onClick={() => handleOpenModal(post)}>
-          <Box fontSize={20} fontWeight="bold" m={3}>
-            {post.CardTitle}
+        <>
+          <Box key={post.id} onClick={() => handleOpenModal()}>
+            <Box fontSize={20} fontWeight="bold" m={3}>
+              {post.CardTitle}
+            </Box>
           </Box>
-        </Box>
-      ))}
 
-      <Modal open={IsModalOpen} onClose={handleCloseModal}>
-        <Box
-          position="absolute"
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          p={4}
-          m={10}
-          bgcolor="white"
-          border="solid 4px"
-          borderRadius="5px"
-        >
-          {selectedPost && (
-            <ItemCardDescription
-              id={selectedPost.id}
-              CardTitle={selectedPost.CardTitle}
-              CardContent={selectedPost.CardContent}
-            />
-          )}
-        </Box>
-      </Modal>
+          <Modal open={IsModalOpen} onClose={handleCloseModal} disableAutoFocus>
+            <Box
+              position="absolute"
+              top="50%"
+              left="50%"
+              p={4}
+              maxHeight={{ xs: "60%", md: "80%" }}
+              minWidth={300}
+              bgcolor="white"
+              borderRadius="5px"
+              sx={{ transform: "translate(-50%, -50%)", overflowY: "scroll" }}
+              // Todo:スクロールバーをかわいくする（余裕があったら！！）
+            >
+              <ItemCardDescription
+                id={post.id}
+                cardTitle={post.CardTitle}
+                posts={post.post}
+              />
+            </Box>
+          </Modal>
+        </>
+      ))}
     </Box>
   );
 };
