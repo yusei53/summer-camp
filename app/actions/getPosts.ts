9e987b7e9d6bcf53c5a4c2b740960da23f0046f3
yusei53 <yusei530922@gmail.com>
2024-09-08 10:28:09 +0900

    getPosts.tsの作成

---
+++ app/actions/getPosts.ts
@@ -0,0 +1,35 @@
+import prisma from "@/app/lib/prisma";
+const getPosts = async (groupId: string) => {
+  try {
+    const posts = await prisma.post.findMany({
+      where: { groupId },
+      orderBy: {
+        id: "asc",
+      },
+      select: {
+        id: true,
+        title: true,
+        descriptions: {
+          orderBy: {
+            id: "desc",
+          },
+          select: {
+            id: true,
+            content: true,
+            category: {
+              select: {
+                name: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    return posts;
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+  }
+};
+
+export default getPosts;
