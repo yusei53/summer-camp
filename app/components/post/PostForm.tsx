553239b89d2d4e938c615a089516446e4e103fd1
togoai0930@icloud.com <togoai0930@icloud.com>
2024-09-09 20:27:53 +0900

    postボタン治った

--- app/components/post/PostForm.tsx
+++ app/components/post/PostForm.tsx
@@ -23,109 +23,108 @@ const PostForm: React.FC<TProps> = ({
   loading,
   categories,
 }) => {
-  const [openPostId, setOpenPostId] = useState<string | null>(null);
+  const [open, setOpen] = useState(false);
 
-  const handleOpenModal = (postId: string) => {
-    setOpenPostId(postId);
+  const handleOpenModal = () => {
+    setOpen(true);
   };
 
   const handleCloseModal = () => {
-    setOpenPostId(null);
+    setOpen(false);
   };
 
   return (
-    <Box
-      component="form"
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="center"
-      mx={{ xs: 2, md: 15 }}
-      mt={5}
-      onSubmit={onSubmit}
-    >
+    <>
       <Button
-        onClick={() => handleOpenModal("new-post")}
+        onClick={handleOpenModal}
         sx={{ mb: 3, color: "white", bgcolor: "blue" }}
       >
         新規作成
       </Button>
 
-      {/* モーダル内に表示するフォーム */}
-      <Modal
-        open={openPostId !== null}
-        onClose={handleCloseModal}
-        disableAutoFocus
-      >
+      <Modal open={open} onClose={handleCloseModal} disableAutoFocus>
         <Box
-          position="absolute"
-          top="50%"
-          left="50%"
-          p={4}
-          maxHeight={{ xs: "60%", md: "80%" }}
-          minWidth={300}
-          bgcolor="white"
-          borderRadius="5px"
-          sx={{
-            transform: "translate(-50%, -50%)",
-            overflowY: "scroll",
-          }}
+          component="form"
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          mx={{ xs: 2, md: 15 }}
+          mt={5}
+          onSubmit={onSubmit}
         >
-          <Typography variant="h5" mb={2} gutterBottom>
-            Post の作成
-          </Typography>
-
-          {/* タイトルフィールド */}
-          <CustomInput
-            id="title"
-            label="タイトル"
-            register={register}
-            disabled={loading}
-            required
-            errors={errors}
-            sx={{ my: 2 }}
-          />
-
-          {/* 各カテゴリに対応する説明フィールド */}
-          {categories.map((field, index) => (
-            <Box
-              key={field.id}
-              display="flex"
-              flexDirection={"column"}
-              alignItems="left"
-              width="100%"
-              my={2}
-            >
-              <Typography variant="body1">{categories[index]?.name}</Typography>
-              <CustomInput
-                id={`descriptions.${index}.content`}
-                label={`説明 ${index + 1}`}
-                register={register}
-                disabled={loading}
-                required
-                errors={errors}
-              />
-              <input
-                type="hidden"
-                {...register(`descriptions.${index}.categoryId`)} // categoryId を hidden フィールドで送信
-                value={field.id}
-              />
-            </Box>
-          ))}
-
-          {/* 送信ボタン */}
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            sx={{ mt: 3, mb: 2, px: { xs: 10, md: 15 } }}
-            disabled={loading}
+          <Box
+            position="absolute"
+            top="50%"
+            left="50%"
+            p={4}
+            maxHeight={{ xs: "60%", md: "80%" }}
+            minWidth={300}
+            bgcolor="white"
+            borderRadius="5px"
+            sx={{
+              transform: "translate(-50%, -50%)",
+              overflowY: "scroll",
+            }}
           >
-            {loading ? "作成中..." : "Post を作成"}
-          </Button>
+            <Typography variant="h5" mb={2} gutterBottom>
+              Post の作成
+            </Typography>
+
+            {/* タイトルフィールド */}
+            <CustomInput
+              id="title"
+              label="タイトル"
+              register={register}
+              disabled={loading}
+              required
+              errors={errors}
+              sx={{ my: 2 }}
+            />
+
+            {/* 各カテゴリに対応する説明フィールド */}
+            {categories.map((field, index) => (
+              <Box
+                key={field.id}
+                display="flex"
+                flexDirection={"column"}
+                alignItems="left"
+                width="100%"
+                my={2}
+              >
+                <Typography variant="body1">
+                  {categories[index]?.name}
+                </Typography>
+                <CustomInput
+                  id={`descriptions.${index}.content`}
+                  label={`説明 ${index + 1}`}
+                  register={register}
+                  disabled={loading}
+                  required
+                  errors={errors}
+                />
+                <input
+                  type="hidden"
+                  {...register(`descriptions.${index}.categoryId`)} // categoryId を hidden フィールドで送信
+                  value={field.id}
+                />
+              </Box>
+            ))}
+
+            {/* 送信ボタン */}
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              sx={{ mt: 3, mb: 2, px: { xs: 10, md: 15 } }}
+              disabled={loading}
+            >
+              {loading ? "作成中..." : "Post を作成"}
+            </Button>
+          </Box>
         </Box>
       </Modal>
-    </Box>
+    </>
   );
 };
 
