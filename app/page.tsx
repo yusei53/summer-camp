2fb90f917bba8de3b76469bb047b3c0a197c4ef5
yusei53 <yusei530922@gmail.com>
2024-09-09 22:31:01 +0900

    /でグループ一覧を表示した

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,15 @@ import getCurrentUser from "../actions/getCurrentUser";
 import GoogleLoginForm from "./components/auth/GoogleLoginForm";
 import LogoutButton from "./components/auth/LogoutButton";
 import GroupFormContainer from "./components/group/GroupFormContainer";
+import GroupList from "./components/group/GroupList";
 
 const Home = async () => {
   const currentUser = await getCurrentUser();
   return currentUser ? (
     <>
-      <LogoutButton />
       <GroupFormContainer />
+      <GroupList currentUserId={currentUser?.id} />
+      <LogoutButton />
     </>
   ) : (
     <GoogleLoginForm />
