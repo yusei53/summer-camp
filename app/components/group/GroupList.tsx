ad3ee7fc025027865a08f97b21a070fcb08c8ad3
YoshiakiAsahara <asa-yossan@outlook.jp>
2024-09-09 17:05:00 +0900

    グループ一覧ページ作成 Co-authored-by: yusei53 <yusei.imamura@sansan.com>

--- app/components/group/GroupList.tsx
+++ app/components/group/GroupList.tsx
@@ -1,5 +1,6 @@
 import getGroups from "@/actions/getGroups";
-import { Box } from "@mui/material";
+import { Box, Card, CardContent, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid2";
 import { User } from "@prisma/client";
 import React from "react";
 
@@ -10,10 +11,44 @@ type TProps = {
 const GroupList: React.FC<TProps> = async ({ currentUserId }) => {
   if (!currentUserId) return;
   const groups = await getGroups(currentUserId);
-  return groups ? (
-    groups.map((group) => <div key={group.id}>{group.groupName}</div>)
-  ) : (
-    <Box>グループがありません</Box>
+
+  if (!groups) return <div>グループがありません</div>;
+
+  return (
+    <Grid
+      container
+      spacing={{ xs: 3, md: 5 }}
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        mx: 10,
+      }}
+    >
+      {groups.map((group) => (
+        <Grid
+          size={{ xs: 12, md: 4 }}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Card
+            key={group.id}
+            sx={{
+              width: 180,
+              height: 180,
+              borderRadius: "16px",
+            }}
+          >
+            <CardContent>
+              <Typography component="div">{group.groupName}</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
