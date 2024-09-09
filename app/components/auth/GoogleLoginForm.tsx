751978e04d69573996331a3f608087d89dfca21d
Sara <sara.k.y.sara@icloud.com>
2024-09-09 23:59:51 +0900

    ログイン画面のレイアウト修正

--- app/components/auth/GoogleLoginForm.tsx
+++ app/components/auth/GoogleLoginForm.tsx
@@ -1,18 +1,37 @@
 import { Box, Typography } from "@mui/material";
 import GoogleLoginButton from "./GoogleLoginButton";
+import EditCalendarIcon from "@mui/icons-material/EditCalendar";
 
 const GoogleLoginForm = () => {
   return (
     <Box
-      mt={15}
+      mt={22}
       mx={{ md: 25 }}
       py={5}
-      px={1}
-      border={"1px solid #c4c4c4"}
+      px={2}
+      // border={"1px solid #c4c4c4"}
       borderRadius={"10px"}
     >
-      <Typography textAlign={"center"} fontWeight={"bold"} fontSize={20}>
-        ここにアプリ名
+      <Box
+        display={"flex"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        flexDirection={"column"}
+      >
+        <EditCalendarIcon
+          sx={{
+            fontSize: 220,
+          }}
+        />
+      </Box>
+      <Typography
+        textAlign={"center"}
+        fontWeight={"bold"}
+        fontSize={20}
+        mt={1}
+        mb={2}
+      >
+        メモパレット
       </Typography>
       <Typography textAlign={"center"} my={4} fontSize={14}>
         ログインすると〇〇の機能が使えるようになります
